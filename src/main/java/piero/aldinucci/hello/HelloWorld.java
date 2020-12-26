2bcf9fd438fd66418756db2b678e818c531611b2
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-12-26 21:38:05 +0100

    random commit

---
+++ src/main/java/piero/aldinucci/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package piero.aldinucci.hello;
+
+public class HelloWorld {
+	public void sayHello() {
+		System.out.println("HelloWorld!");
+	}
+
+}
---
+++ src/test/java/piero/aldinucci/hello/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package piero.aldinucci.hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		HelloWorld obj = new HelloWorld();
+		obj.sayHello();
+	}
+
+}
